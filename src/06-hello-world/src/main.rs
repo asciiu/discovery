292ba8f56079382913c667e1e6a2d25129cc3381
Flowmaster <ellyssin.gimhae@gmail.com>
2019-10-15 21:00:00 -0600

    Build target should be thumbv7em-none-eabihf for cargo run. Also added some debug stuff to openocd.

--- src/06-hello-world/src/main.rs
+++ src/06-hello-world/src/main.rs
@@ -7,9 +7,10 @@ use aux6::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let mut itm = aux6::init();
+    //let mut itm = aux6::init();
 
-    iprintln!(&mut itm.stim[0], "Hello, world!");
+    //iprintln!(&mut itm.stim[0], "Hello, world!");
 
-    loop {}
+    //loop {}
+    panic!("Hello, world!");
 }
